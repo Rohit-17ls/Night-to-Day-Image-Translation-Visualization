403ec6c22126e2950fdd7a29855c6418c0f5bf17
Rohit <rohitd.arsly@gmail.com>
2024-04-04 01:34:36 +0530

    Added download.sh

---
+++ app/app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import io
+from PIL import Image
+import torchvision.transforms as transforms
+import model
+
+model_store = model.GAN_Model_Store()
+
+def update_selected_model():
+    GAN_model = model_store.get_model(selected_model);    
+
+
+st.header("Night to Day Image Translation using GANs");
+
+selected_model = st.selectbox("Choose GAN model", 
+                             ("CycleGAN", "DiscoGAN", "UNITGAN"),
+                             on_change = update_selected_model);
+
+GAN_model = model_store.get_model(selected_model);
+
+st.write(f"Selected Model : {selected_model}")
+
+
+uploaded_file = st.file_uploader("Pick Image", accept_multiple_files = False);
+
+if(uploaded_file is not None):
+    image_cols = st.columns(2);
+    
+    img = Image.open(uploaded_file)
+    img = transforms.Resize((128, 128), Image.BICUBIC)(img)
+    height, width = img.size
+
+    
+
+    with image_cols[0]:
+        st.image(img);
+        st.write("Input Image");
+
+
+
+    with image_cols[1]:
+        translated_img = model.translate(img, GAN_model);
+        st.image(translated_img)
+        st.write("Translated Image");
+
+    
---
+++ app/download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://bitbucket.org/developer-workspace/data/raw/e68253945a5a19ea8b9a8897281a1bf4593da646/models/CycleGAN.pth -P ./assets
+wget https://bitbucket.org/developer-workspace/data/raw/e68253945a5a19ea8b9a8897281a1bf4593da646/models/DiscoGAN.pth -P ./assets
+wget https://bitbucket.org/developer-workspace/data/raw/e68253945a5a19ea8b9a8897281a1bf4593da646/models/UNITGAN.pth -P ./assets
