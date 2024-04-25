b02412bc10461631d66863f757b9f877648c9d2f
Rohit <rohitd.arsly@gmail.com>
2024-04-26 01:46:04 +0530

    Updated download.sh to allow for downloading DualGAN and ToDayGAn

--- app/download.sh
+++ app/download.sh
@@ -3,3 +3,5 @@
 wget https://bitbucket.org/developer-workspace/data/raw/e68253945a5a19ea8b9a8897281a1bf4593da646/models/CycleGAN.pth -P ./assets
 wget https://bitbucket.org/developer-workspace/data/raw/e68253945a5a19ea8b9a8897281a1bf4593da646/models/DiscoGAN.pth -P ./assets
 wget https://bitbucket.org/developer-workspace/data/raw/e68253945a5a19ea8b9a8897281a1bf4593da646/models/UNITGAN.pth -P ./assets
+wget https://bitbucket.org/developer-workspace/data2/raw/5e69846a504ef201515ff15ab6de89dfea63fb04/models/DualGAN.pth -P ./assets
+wget https://bitbucket.org/developer-workspace/data2/raw/5e69846a504ef201515ff15ab6de89dfea63fb04/models/ToDayGAN.pth -P ./assets
